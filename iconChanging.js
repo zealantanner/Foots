683695ed23c88bba7dd717be1434650cc435a18a
zealantanner <zealantanner@gmail.com>
2021-10-23 21:24:52 -0600

    made separate javascript file for website icon changing

---
+++ iconChanging.js
@@ -0,0 +1,18 @@
+function randomFootsPic(x){
+    switch(x) {
+        case 1: return "footsPics/foot icon good.svg";
+        case 2: return "footsPics/FootIcon.png";
+        default: return "footsPics/FootIcon2.svg";
+    }
+}
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+
+$('#button').on('click', function() {
+    console.log(getRandomInt(3));
+    $('head #icon').replaceWith(`
+        <link id="icon" rel="icon" href="${randomFootsPic(getRandomInt(3))}">
+    `);
+});
