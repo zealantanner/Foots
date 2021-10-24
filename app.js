0902985868f9d25d7513ee851bb9b22edb52f9e1
zealantanner <zealantanner@gmail.com>
2021-10-23 18:54:07 -0600

    Added foots pics

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-
-
-function toggleDark(a){document.getElementById('darkModeButton')
-    .addEventListener(a, function(){
-	    $('body').toggleClass('dark');
-    })
+function randomFootsPic(x){
+    switch(x) {
+        case 1: return "footsPics/foot icon good.svg";
+        case 2: return "footsPics/FootIcon.png";
+        default: return "footsPics/FootIcon2.svg";
+    }
 }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-toggleDark('click'); //normal mode
+// $( ".inner" ).prepend( "<p>Test</p>" );
 
+$('#button').on('click', function() {
+    console.log(getRandomInt(3));
+    $('head #icon').replaceWith(`
+        <link id="icon" rel="icon" href="${randomFootsPic(getRandomInt(3))}">
+    `);
+    // $('head #icon').replaceWith(`
 
-// toggleDark('mousemove'); //seizure mode
+});
