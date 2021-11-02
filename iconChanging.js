cf31835c3bffbb891af96d93efbd87f62cf2ac35
zealantanner <zealantanner@gmail.com>
2021-11-02 02:05:37 -0600

    Messing with icon stuff

--- iconChanging.js
+++ iconChanging.js
@@ -1,18 +1,26 @@
 function randomFootsPic(x){
     switch(x) {
-        case 1: return "footsPics/foot icon good.svg";
-        case 2: return "footsPics/FootIcon.png";
-        default: return "footsPics/FootIcon2.svg";
+        case 3: return "FootIcon2.svg";
+        case 2: return "FootIcon1.svg";
+        case 1: return "FootIcon.svg";
+        default: return "FootIcon.png";
     }
 }
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
+function changeIconToRandomFoot(footNumber) {
+    $('head #icon').replaceWith(`
+        <link id="icon" rel="icon" href="footsPics/${randomFootsPic(footNumber)}">
+    `);
+}
+var fs = require('./');
+var files = fs.readdirSync('/downloads');
+
 
 $('#button').on('click', function() {
-    console.log(getRandomInt(3));
-    $('head #icon').replaceWith(`
-        <link id="icon" rel="icon" href="${randomFootsPic(getRandomInt(3))}">
-    `);
+    changeIconToRandomFoot(getRandomInt(3));
 });
+
+//on start up
